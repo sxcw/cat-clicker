0e2f934b60ad1f1055f44d1d509ef862768c1fc3
sxcw <yuan329419@gmail.com>
2016-03-03 09:32:53 -0600

    WIP: work on catListView and catView, click count still has issues

--- app.js
+++ app.js
@@ -1,106 +1,79 @@
 $(function(){
 	var model = {
+
 		cats: ["Silvia", "Golphy", "Cutie", "Sadie", "Naughty"],
 
 			// data in displayArea
 		catInfo: {
-			"Silvia":
-				{
+			"Silvia":{
 					"pic" : "silvia.jpg",
 					"numClick" : 0
-				},
-			"Golphy":
-				{
+			},
+			"Golphy":{
 					"pic" : "golphy.jpg",
 					"numClick" : 0
-				},
-			"Cutie":
-				{
+			},
+			"Cutie":{
 					"pic" : "cutie.jpg",
 					"numClick" : 0
-				},
-			"Sadie": 
-				{
+			},
+			"Sadie": {
 					"pic" : "sadie.jpg",
 					"numClick" : 0
-				},
-			"Naughty": 
-				{
+			},
+			"Naughty": {
 					"pic" : "naughty.jpg",
 					"numClick" : 0
-				}						
+			}						
 		}	
-
-
 	};
 
 	var octopus = {
-		init: function() {			
+
+		init: function(){			
 			catListView.init();
 			catView.init();
 		},
+
 		getCats: function(){
 			return model.cats;
 		},
+
 		getCatInfo: function(){
 			return model.catInfo;
 		},
-		setCurrentCat: function(){
+
+		incrementCounter: function(catName){
+			// var cats = getCats();
+			// var catInfo = getCatInfo();
+			// for (i = 0; i < cats.length; i++) {		
+			// 	// clear out previous click count text and increment the click count number when user clicks on the image
+			// 	document.getElementById("catPic").onclick = function(){
+			// 		$("#catNumClick").text("");
+			// 		catInfo[catID].numClick++;
+			// 		$("#catNumClick").append("Number of clicks: "+ catInfo[catID].numClick);
+			// 	}
+			// }
+			return model.catInfo[catName].numClick++; 
 
 		},
-		incrementCounter: function(){
-			var cats = getCats();
-			var catInfo = getCatInfo();
-			for (i = 0; i < cats.length; i++) {		
-				// clear out previous click count text and increment the click count number when user clicks on the image
-				document.getElementById("catPic").onclick = function(){
-					$("#catNumClick").text("");
-					catInfo[catID].numClick++;
-					$("#catNumClick").append("Number of clicks: "+ catInfo[catID].numClick);
-				}
-			}
-		},
-		// selectCat: function() {
-		
-
-		// 	for (i = 0; i < cats.length; i++) {				
-		// 		// add event listener 
-		// 		catElementLink.addEventListener('click', function(){
-		// 			//hide the hint
-		// 			$("#hint").hide();
-		// 			//get id
-		// 			var catID = $(this).attr('id');
-
-		// 			// clear out previous text
-		// 			$("#catName").text("");
-		// 			$("#catPic").text("");
-		// 			$("#catNumClick").text("");
-
-		// 			// append name, pic and click count
-		// 			$("#catName").append("Cat name: " +catID);
-		// 			$("#catNumClick").append("Number of clicks: "+ catInfo[catID].numClick); 
-		// 			$("#catPic").append('<img src="'+  catInfo[catID].pic +'">');
-		// 		});
-		// 	}
-		// },
-		addClickCount: function(){
-			
-		
-		}
 	};
 
 	var catListView = {
+		// cats: undefined,
+		// catInfo: undefined,
 		// init gets called once
 		init: function(){
 			this.cats = octopus.getCats();
 			this.catInfo = octopus.getCatInfo();
 			console.log(this.catInfo);
-
 			this.render();							
 		},
+
 		render: function(){
+			console.log(this);
+			var catListView = this; 
 			//create catSelection and displayArea
-			this.catInfo = octopus.getCatInfo();
 			for (i = 0; i < this.cats.length; i++) {
 				var cat = this.cats[i];
 
@@ -122,9 +95,10 @@ $(function(){
 
 					$("#hint").hide();
 					//get id
-					var catID = $(this).attr('id');console.log(this.catInfo);
-					console.log("catID is "+ catID);
-					console.log(this.catInfo[catID]);
+					var catID = $(this).attr('id');
+					// console.log("catID is "+ catID);
+					// console.log(catListView);
+					// console.log(catListView.catInfo[catID]);
 
 					// clear out previous text
 					$("#catName").text("");
@@ -133,32 +107,41 @@ $(function(){
 
 					// append name, pic and click count
 					$("#catName").append("Cat name: " +catID);
-					console.log(this.catInfo);
-					$("#catNumClick").append("Number of clicks: "+ this.catInfo[catID].numClick); 
-					$("#catPic").append('<img src="'+  this.catInfo[catID].pic +'">');
-				})
-						
-				
+					// console.log(this.catInfo);
+					$("#catNumClick").append("Number of clicks: "+ catListView.catInfo[catID].numClick); 
+					$("#catPic").append('<img src="'+  catListView.catInfo[catID].pic +'">');
+				})									
 			}	
 		}
 	};
 
 	var catView = {
+
 		init: function(){
 			// create display area
 			var display = document.createElement('div');
 			display.className = 'displayArea';
 
 			// repetition!!!
-			this.cats = octopus.getCats();
-			this.catInfo = octopus.getCatInfo();
-			this.catElementLink = document.createElement('a');
+			// this.cats = octopus.getCats();
+			// this.catInfo = octopus.getCatInfo();
+			// this.catElementLink = document.createElement('a');
 			this.render();
 		},
+
 		render: function(){	
-
-
+			// this.catInfo = octopus.getCatInfo();
+			// var catID = octopus.getCats();
+			// console.log("catID is:"+ catID);
+			// var catPic = this.catInfo["catID"].pic;
+			var counter = document.getElementById("catNumClick");
+			console.log(this);
+			$("#catPic")[0].addEventListener('click', function(){
+				$(counter).text("");
+				$(counter).append("Number of clicks: " + octopus.incrementCounter(catName));
+			});
 		}
 	}	
+
 	octopus.init();
 });	
