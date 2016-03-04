da4d37ea5d91a4f7f116475eafa7127f5b0a1dc9
sxcw <yuan329419@gmail.com>
2016-03-04 09:18:53 -0600

    Format code and add comments

--- app.js
+++ app.js
@@ -19,40 +19,42 @@ $(function(){
 	};
 
 	var octopus = {
+
 		// init runs once
 		init: function(){
 			view.init();
 		},
+
 		// return number of cats
 		getNumOfCats: function(){
 			return model.cats.length;
 		},
+
 		// return a cats array object, cats[0] is Silvia
 		getCatInfo: function(num){ 
 			return model.cats[num];
 		},
+
 		//return clickCount of a cat
 		getCount: function(num){
 			return model.cats[num].clickCount;
 		}
-
 	};
 
 	var view = {
+
 		// set up menu and display area
 		init: function(){
 
-			this.linkNameArray =[];
-			var catNum = octopus.getNumOfCats(); // number of menu items
+			this.catNum = octopus.getNumOfCats(); // number of menu items
 			this.catName = document.getElementById("catName"); //store DOM into variables for easy access
 			this.catPic = document.getElementById("catPic");
 			this.catNumClick = document.getElementById("catNumClick");
 
 			// create menu 
-			for (i = 0; i<catNum; i++){
+			for (i = 0; i<this.catNum; i++){
 				this.linkName = octopus.getCatInfo(i).id;
 				$("#catSelection").append("<li><a href = '#' class = 'catLink'>" + this.linkName + "</a></li>");
-				this.linkNameArray.push(this.linkName);
 			}
 
 			// display Silvia initially 
@@ -62,23 +64,31 @@ $(function(){
 			
 			this.render();
 		},
-		// render 
+
+		// render: listen to click events
 		render: function(){
 
-			// I want "this" points to the same "this" as in "this.catName" where "this" refers to "view" itself. It makes sense to name it "view"
+			// I want "this" points to the same "this" as in "this.catName" where "this" refers to "view" itself. It makes sense to declare the variable as "view"
 			var view = this; 
-			for(var i = 0; i< this.linkNameArray.length; i++){
+
+			// for loop creates event listener for all the menu links. 
+			for(var i = 0; i< this.catNum; i++){
+				
+				//Have to create a closure and redefine "i" inside so that the console won't throw undefined error, there is no getCatInfo(i).id if the "i" still bounds to outer scope value
 				(function(){
 					var index = i;
 
+					// Use immediately invoked function so that it will get run. (otherwise it won't because we don't call the function explicitly)
 					$(".catLink")[index].addEventListener('click',function(){
-						console.log(index);
-						console.log(view);
+						$(view.catName).text("");
+						$(view.catPic).text("");
+						$(view.catNumClick).text("");
 						$(view.catName).append("Cat Name: "+ octopus.getCatInfo(index).id);
 						$(view.catPic).append("<img src='images/"+ octopus.getCatInfo(index).pic+"'>");
 						$(view.catNumClick).append("Number of Clicks: "+octopus.getCount(index));
 					});
 				}());
+
 			}
 
 		}				
